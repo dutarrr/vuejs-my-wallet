074b32060b40aac5072aa9030cfd882b9f975f61
dutarrr <duzguntutar@gmail.com>
2021-06-12 17:17:54 +0300

    bootstrap and file organazing

---
+++ app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="container">
+      <a href="#" class="btn btn-primary">Primary</a>
+      <a href="#" class="btn btn-secondary">Secondary</a>
+      <a href="#" class="btn btn-success">Success</a>
+      <a href="#" class="btn btn-info">Info</a>
+      <a href="#" class="btn btn-warning">Warning</a>
+      <a href="#" class="btn btn-danger">Danger</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+</style>
