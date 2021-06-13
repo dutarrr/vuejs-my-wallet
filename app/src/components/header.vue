5494ff16f64536bd59c3a0ffa6fbb932ed2e5bb4
dutarrr <duzguntutar@gmail.com>
2021-06-13 20:13:26 +0300

    css style nav

--- app/src/components/header.vue
+++ app/src/components/header.vue
@@ -7,13 +7,21 @@
 
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <router-link to="/" tag="li" class="nav-item active"
+          <router-link to="/" tag="li" class="nav-item" active-class="active"
             ><a class="nav-link">Home</a></router-link
           >
-          <router-link to="/currency" tag="li" class="nav-item"
+          <router-link
+            to="/currency"
+            tag="li"
+            class="nav-item"
+            active-class="active"
             ><a class="nav-link">Döviz</a></router-link
           >
-          <router-link to="/emtia" tag="li" class="nav-item"
+          <router-link
+            to="/emtia"
+            tag="li"
+            class="nav-item"
+            active-class="active"
             ><a class="nav-link">Emtia</a></router-link
           >
         </ul>
@@ -21,3 +29,13 @@
     </div>
   </nav>
 </template>
+
+<style scoped>
+nav {
+  border-radius: 10px;
+  box-shadow: 3px 3px #999;
+}
+img {
+  border-radius: 10px;
+}
+</style>>
