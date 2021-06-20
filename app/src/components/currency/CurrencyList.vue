e9ac0072b7578f615e64b97bd3a6d642e352b230
dutarrr <duzguntutar@gmail.com>
2021-06-20 22:27:11 +0300

    currency list get data from state

--- app/src/components/currency/CurrencyList.vue
+++ app/src/components/currency/CurrencyList.vue
@@ -4,34 +4,30 @@
       <tr>
         <th scope="col">#</th>
         <th scope="col">Alış TL</th>
-        <th scope="col">Stış TL</th>
+        <th scope="col">Satış TL</th>
         <th scope="col">Alış Kur</th>
         <th scope="col">Satış Kur</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-        <td>@mdo</td>
+      <tr v-for="currency in getAllCurrency" :key="currency.id">
+        <th scope="row">{{ currency.id }}</th>
+        <td>{{ currency.alisTl }}</td>
+        <td>{{ currency.satisTl }}</td>
+        <td>{{ currency.alisKur }}</td>
+        <td>{{ currency.satisKur }}</td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
-//import { mapGetters } from "vuex";
+import { mapGetters } from "vuex";
 
 export default {
   created() {},
+  computed: {
+    ...mapGetters(["getAllCurrency"]),
+  },
 };
 </script>
